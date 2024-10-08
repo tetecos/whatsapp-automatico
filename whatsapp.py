7d820fc922885bb9f72c248927df17835d409de9
tetecos <brosmega69@gmail.com>
2024-10-08 20:32:14 -0300

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,15 +1,15 @@
 import os
 import pywhatkit
 import time
-numero_big = "+351 910 237 127"
+numero = "+xx xxxxxxxx"
 
-imagepath = "C:/Users/T-GAMER/Pictures/leandro hassum.jfif"
+imagepath = "image path mesmo"
 
 os.system('cls')
 contador = 0
 
 while contador < 5:
-    pywhatkit.sendwhats_image(numero_big, imagepath,"joao dolinsky hasum",10,True)
+    pywhatkit.sendwhats_image(numero, imagepath,"joao dolinsky hasum",10,True)
 
     time.sleep(5)
     contador += 1
